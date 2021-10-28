c5b3cb2e698e8459bf4b847039052a409d59a62a
SerG-I0 <sergio231199@hotmail.com>
2021-10-28 11:53:57 +0200

    ActuTests

--- src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaCrearCentro.java
+++ src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaCrearCentro.java
@@ -2,18 +2,27 @@ package uclm.grupo2.sigeva.cucumber.pruebas;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+//import uclm.grupo2.sigeva.http.CentroSaludController;
+//import uclm.grupo2.sigeva.model.CentroSalud;
 import io.cucumber.java.en.Then;
 
 public class PruebaCrearCentro {
 	
 	@Given("un {string}, {string}, {string} y {string}")
 	public void un_y(String nombreCentro, String direccionCentro, String numeroVacunas, String numeroSanitarios) {
-	    // Write code here that turns the phrase above into concrete actions
+		/*CentroSalud centro = new CentroSalud();
+		nombreCentro= "Zendal"; direccionCentro="Avenida Parque"; numeroVacunas="7780"; numeroSanitarios="123";  
+		centro.setnombreCentro(nombreCentro);
+		centro.setdireccionCentro(direccionCentro);
+		centro.setnumeroVacunas(numeroVacunas);
+		centro.setnumetoSanitarios(numeroSanitarios);
+		CentroSaludController CentroCtrl = new CentroSaludController();
+		CentroCtrl.insertarCentro(centro);*/
 	}
 
 	@Then("se crea un centro de salud")
 	public void se_crea_un_centro_de_salud() {
-	    // Write code here that turns the phrase above into concrete actions
+
 	}
 
 }
