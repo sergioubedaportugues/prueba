96a4a35c6c6c0d52c0d9a9786e7f75d2c55b3ab9
SerG-I0 <sergio231199@hotmail.com>
2021-11-01 20:29:23 +0100

    Fix Tests

--- src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaCrearCentro.java
+++ src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaCrearCentro.java
@@ -1,14 +1,11 @@
 package uclm.grupo2.sigeva.cucumber.pruebas;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 import uclm.grupo2.sigeva.http.CentroSaludController;
 import uclm.grupo2.sigeva.model.CentroSalud;
-import uclm.grupo2.sigeva.dao.CentroSaludDAO;
 import io.cucumber.java.en.Then;
 
-public class PruebaCrearCentro {
-	
+public class PruebaCrearCentro{
 	@Given("un {string}, {string}, {string}, {string}, {string} y {string}")
 	public void un_y(String nombreCentro, String direccionCentro, String numeroVacunas, String fInicio, String fFin, String cupo) {
 		
@@ -16,6 +13,7 @@ public class PruebaCrearCentro {
 		CentroSalud centro = new CentroSalud(nombreCentro,direccionCentro,numeroVacunas,fInicio,fFin,cupo);
 		CentroSaludController CentroCtrl = new CentroSaludController();
 		CentroCtrl.insertarCentro(centro);
+		
 	}
 
 	@Then("se crea un centro de salud")
