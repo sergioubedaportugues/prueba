996ddead1cffe1c596c465899ae3f73e7928b56c
SerG-I0 <sergio231199@hotmail.com>
2021-11-02 19:28:59 +0100

    Test Modificacion
    Se han añadido test a las funcionalidades de modificacion de Centros y Usuarios

--- src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaModificarCentro.java
+++ src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaModificarCentro.java
@@ -1,34 +1,46 @@
 package uclm.grupo2.sigeva.cucumber.pruebas;
 
+import static org.junit.Assert.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
+import uclm.grupo2.sigeva.http.CentroSaludController;
+import uclm.grupo2.sigeva.model.CentroSalud;
 import io.cucumber.java.en.Then;
 
 public class PruebaModificarCentro {
+	
+	@Autowired
+	private CentroSaludController CentroCtrl;
 
-	@Given("los datos de {string}, {string} y {string}")
-	public void los_datos_de_y(String nombreCentro, String direccionCentro, String numeroVacunas) {
-	    
-		/*  Pasos para insertar un NUEVO CENTRO si se ejecutan en orden los test no hace falta
-		CentroSalud centro = new CentroSalud();
-		nombreCentro= "Zendal"; direccionCentro="C/Avenida Parque 8"; numeroVacunas="7780";  
-		centro.setnombreCentro(nombreCentro);
-		centro.setdireccionCentro(direccionCentro);
-		centro.setnumeroVacunas(numeroVacunas);;
-		CentroSaludController CentroCtrl = new CentroSaludController();
-		CentroCtrl.insertarCentro(centro);
-		*/ 
+	@Given("los datos de {string}, {string}, {string}, {string}, {string} y {string}")
+	public void los_datos_de_y(String nombreCentro, String direccionCentro, String numeroVacunas, String fInicio, String fFin, String cupo) {
+		nombreCentro= "Miguelturra33"; direccionCentro="Avenida Parque 8"; numeroVacunas="7780"; fInicio = "09:30"; fFin = "14:00"; cupo="4";
+		String nombreCentroN= "Miguelturra34"; String direccionCentroN="Avenida Parque 6"; String numeroVacunasN="8780";
+		String fInicioN = "09:30"; String fFinN = "14:00";String cupoN="4";
 		
-		/* Modificado del centrol
-		String direccionCentroN;
-		centro.setdireccionCentro(direccionCentroN);
-		CentroCtrl.modificarCentro(centro);
-		*/
+		CentroSalud centro = new CentroSalud();
+		centro.setNombre(nombreCentro);
+		centro.setDireccion(direccionCentro);
+		centro.setNumVacunas(numeroVacunas);
+		centro.setfInicio(fInicio);
+		centro.setfFin(fFin);
+		centro.setCupo(cupo);
+		CentroCtrl.insertarCentro(centro);
+		
+		centro.setNombre(nombreCentroN);
+		centro.setDireccion(direccionCentroN);
+		centro.setNumVacunas(numeroVacunasN);
+		centro.setfInicio(fInicioN);
+		centro.setfFin(fFinN);
+		centro.setCupo(cupoN);
+		
+		assertEquals("Centro modificado",CentroCtrl.modificarCentro(centro));
 	}
-
 
 	@Then("se ha modificado la informacion del centro")
 	public void se_ha_modificado_la_informacion_del_centro() {
 
-	}	
+	}
+	
 }
