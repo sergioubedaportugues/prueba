6a24ebb039a87a3376acf2cefd2e0be4c57a0f93
SerG-I0 <sergio231199@hotmail.com>
2021-11-10 14:55:26 +0100

    Fix Centros Test

---
+++ src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaModificarDatosCentro.java
@@ -0,0 +1,43 @@
+package uclm.grupo2.sigeva.cucumber.pruebas;
+
+import static org.junit.Assert.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import io.cucumber.java.en.Given;
+import uclm.grupo2.sigeva.http.CentroSaludController;
+import uclm.grupo2.sigeva.model.CentroSalud;
+import io.cucumber.java.en.Then;
+public class PruebaModificarDatosCentro {
+	
+
+	@Autowired
+	private CentroSaludController CentroCtrl;
+	
+	@Given("los datos asociados {string}, {string}, {string}, {string}, {string}, {string} y {string}")
+	public void los_datos_asociados_y(String nombreCentro, String direccionCentro, String numeroVacunas, String fInicio, String fFin, String franja, String cupo) {
+		nombreCentro= "Miguelturra33"; direccionCentro="Avenida Parque 8"; numeroVacunas="7780"; fInicio = "09:30"; fFin = "14:00"; franja= "6"; cupo="5";
+		String nombreCentroN= "Miguelturra34"; String direccionCentroN="Avenida Parque 6"; String numeroVacunasN="8780";
+		
+		CentroSalud centro = new CentroSalud();
+		centro.setNombre(nombreCentro);
+		centro.setDireccion(direccionCentro);
+		centro.setNumVacunas(numeroVacunas);
+		centro.setfInicio(fInicio);
+		centro.setfFin(fFin);
+		centro.setFranja(franja);
+		centro.setCupo(cupo);
+		CentroCtrl.insertarCentro(centro);
+		
+		centro.setNombre(nombreCentroN);
+		centro.setDireccion(direccionCentroN);
+		centro.setNumVacunas(numeroVacunasN);
+		
+		assertEquals("Centro modificado",CentroCtrl.modificarCentro(centro));
+		CentroCtrl.borrarCentro(centro);
+	}
+
+	@Then("se ha modificado la informacion del centro correctamente")
+	public void se_ha_modificado_la_informacion_del_centro_correctamente() {
+
+	}
+}
