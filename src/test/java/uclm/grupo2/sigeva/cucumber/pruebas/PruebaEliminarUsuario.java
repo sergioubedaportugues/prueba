57522f8444a69a19e39d0cf165ee3bc2bf91a49b
Christopher Pirez <Christopher.Pirez@alu.uclm.es>
2021-11-11 12:28:06 +0100

    Rectificación Pruebas

--- src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaEliminarUsuario.java
+++ src/test/java/uclm/grupo2/sigeva/cucumber/pruebas/PruebaEliminarUsuario.java
@@ -18,7 +18,7 @@ public class PruebaEliminarUsuario {
 	@Given("un usuario con {string}, {string}, {string}, {string}, {string}, {string} y {string}")
 	public void un_usuario_con_y(String login, String password, String nombre, String apellidos, String telefono, String dni, String rol) {
 		Usuario user = new Usuario();
-		login= "Antonio68"; password="Patata68"; nombre="Antonio"; apellidos="Fernandez"; telefono="888888878"; dni="98888888A"; rol="Admin"; 
+		login= "Antonio65"; password="Patata68"; nombre="Antonio"; apellidos="Fernandez"; telefono="888888878"; dni="98888888A"; rol="Admin"; 
 		user.setLogin(login);
 		user.setNombre(nombre);
 		user.setPassword(password);
