c4eb2148e4f10f64ce3df1a5af28ddbaab783514
ramongalera <ramon.galera@alu.uclm.es>
2021-11-17 19:23:02 +0100

    Update con las demas ramas

--- src/main/java/uclm/grupo2/sigeva/http/SanitarioController.java
+++ src/main/java/uclm/grupo2/sigeva/http/SanitarioController.java
@@ -1,10 +1,14 @@
 package uclm.grupo2.sigeva.http;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uclm.grupo2.sigeva.dao.CitasDAO;
+import uclm.grupo2.sigeva.model.Citas;
 
 @RestController
 @RequestMapping("vistaSanitario")
@@ -16,4 +20,8 @@ public class SanitarioController {
 	// ConsultarCitas
 	// AplicarVacuna
 	
+	/*@GetMapping("/getCitasPorCentro/{login}")
+	public List<Citas> getCitasPorCentro(){
+		return cita.getByDiaAndCs(fecha, );
+	}*/
 }
