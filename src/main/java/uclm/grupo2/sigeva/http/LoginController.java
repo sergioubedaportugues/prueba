e0fa92ec6122e0e245925d9321be228d56c8ef65
sergioubedaportugues <72705830+sergioubedaportugues@users.noreply.github.com>
2021-11-17 22:17:21 +0100

    update

--- src/main/java/uclm/grupo2/sigeva/http/LoginController.java
+++ src/main/java/uclm/grupo2/sigeva/http/LoginController.java
@@ -2,22 +2,14 @@ package uclm.grupo2.sigeva.http;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-
 import java.util.List;
-
 import org.apache.commons.codec.digest.DigestUtils;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.server.ResponseStatusException;
-
 import uclm.grupo2.sigeva.dao.UsuarioDAO;
 import uclm.grupo2.sigeva.exceptions.CredencialesInvalidasException;
 import uclm.grupo2.sigeva.model.Usuario;
