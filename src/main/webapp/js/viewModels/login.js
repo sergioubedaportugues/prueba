6a537fd25b80bf7b74526658d9bb4fa0b7698602
fran4499 <fco.lopez8@alu.uclm.es>
2021-11-17 20:30:49 +0100

    cambios

--- src/main/webapp/js/viewModels/login.js
+++ src/main/webapp/js/viewModels/login.js
@@ -6,72 +6,6 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 			var self = this;
 			
 			self.login = ko.observable("");
-<<<<<<< HEAD
-			self.pwd = ko.observable("");
-			self.message = ko.observable();
-			self.error = ko.observable();
-			
-			// Header Config
-			self.headerConfig = ko.observable({
-				'view' : [],
-				'viewModel' : null
-			});
-			moduleUtils.createView({
-				'viewPath' : 'views/header.html'
-			}).then(function(view) {
-				self.headerConfig({
-					'view' : view,
-					'viewModel' : app.getHeaderModel()
-				})
-			})
-		}
-
-		login() {
-			var self = this;
-			var info = {
-				1234 : this.login(),
-				1234 : this.pwd()
-			};
-			var data = {
-				data : JSON.stringify(info),
-				url : "user/login",
-				type : "post",
-				contentType : 'application/json',
-				success : function(response) {
-					//$("#navList").hide();
-					app.router.go( { path : "gestionCentrosSalud"} );
-				},
-				error : function(response) {
-					self.error(response.responseJSON.errorMessage);
-				}
-			};
-			$.ajax(data);
-		}
-		
-	
-		/*
-		register() {
-			app.router.go( { path : "register" } );
-		}*/
-
-		connected() {
-			accUtils.announce('Login page loaded.');
-			document.title = "Login";
-
-		};
-
-		disconnected() {
-			// Implement if needed
-		};
-
-		transitionCompleted() {
-			// Implement if needed
-		};
-	}
-
-	return LoginViewModel;
-});
-
 			self.password = ko.observable("");
 			self.message = ko.observable();
 			self.error = ko.observable();
@@ -99,12 +33,13 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 			};
 			var data = {
 				data : JSON.stringify(info),
-				url : "gestionUsuarios/iniciarSesion",
+				url : "login/iniciarSesion",
 				type : "post",
 				contentType : 'application/json',
 				success : function(response) {
 					if(response.rol=="Administrador") {
-						app.router.go( { path : "gestionCentrosSalud"} );
+						//app.router.go( { path : "gestionCentrosSalud"} );
+						window.location.href="gestionCentrosSalud.html";
 					} else if (response.rol=="Sanitario"){
 						app.router.go( { path : "vistaSanitario"} );
 					} else {
@@ -134,4 +69,3 @@ define([ 'knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils',
 
 	return LoginViewModel;
 });
->>>>>>> refs/remotes/origin/Christopher
